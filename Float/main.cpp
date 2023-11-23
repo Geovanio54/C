2ade50f770e7ab6d9e8ae7aba353d130956cb7d8
Geovanio54 <mitozinbr@gmail.com>
2023-11-22 21:01:17 -0300

    C++

---
+++ Float/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float numero = 3.5E2;
+
+    //  3.5E2 é a mesma coisa que 3.5*10**2
+
+    cout << numero << endl;
+    
+    return 0;
+}
