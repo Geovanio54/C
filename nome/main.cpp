dd846d5fdb31d954226bbbed588374d5c45ba179
Geovanio54 <mitozinbr@gmail.com>
2023-11-23 16:46:07 -0300

    C++

---
+++ nome/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    string nome;
+
+    cout << "Digite o seu nome: " << endl;
+    cin >> nome;
+    cout << "Prazer em te conhecer " << nome << endl;
+
+    return 0;
+}
