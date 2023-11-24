5f3a764cd9bb0f89fe7ac9385a0fcecc92ddeea6
Geovanio54 <mitozinbr@gmail.com>
2023-11-24 00:53:03 -0300

    C++

---
+++ biblioteca_math.h/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n, raiz;
+
+    cout << "Digite um numero: " << endl;
+    cin >> n;
+
+    raiz = sqrt(n);
+
+    cout << "A raiz quadrata de " << n << " é " << raiz << endl;
+
+    return 0;
+}
