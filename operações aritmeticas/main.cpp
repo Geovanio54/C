32fd3a2bf3b1a76c4854fadc517b783ddfddd224
Geovanio54 <mitozinbr@gmail.com>
2023-11-23 23:54:56 -0300

    C++

--- operações aritmeticas/main.cpp
+++ operações aritmeticas/main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 int main()
 {
 
-    float x, y;
-    x = 5;
+    int x, y;
+    x = 6;
     y = 2;
 
     cout << "O valor de x e y é " << x << " e " << y << " repectivamente" << endl;
     cout << "\tA adição de x e y é " << x + y << endl;
     cout << "\tA subtração de x e y é " << x - y << endl;
     cout << "\tA multiplicação de x e y é " << x * y << endl; 
+    cout << "\tResto da divisão de x e y " << x % y << endl; 
+
+    // % só funciona se a variavel for do tipo inteiro. Caso contrario ele não faz modulo(%)
+    // mais se colocar o valor direto ele consegue fazer o modulo
+    // Ex 5 / 2 == 2.5           Ex 5 % 2 == 1
 
     // \t da um TAB no seu código
     // \n quebra a linha do seu código
